a369bbf3f7762d1c8373932b559dc29b3d4f226a
Daniel Tooby <danieltooby@yahoo.com>
2023-02-21 12:00:12 +0000

    Moved Scripts to Folder

---
+++ Assets/Scripts/Test_Scriptable_Object.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptableObject", order = 2, fileName = "New Script Object")]
+public class Test_Scriptable_Object : ScriptableObject
+{
+    public string Test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

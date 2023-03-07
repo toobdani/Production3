384496928a4bfdc106383b75f91dff0040a4274c
Daniel Tooby <danieltooby@yahoo.com>
2023-03-07 16:04:32 +0000

    Started Portal Pefab/Orgonised Files

---
+++ Assets/Portal_Code.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Portal_Code : MonoBehaviour
+{
+    [SerializeField] GameObject Portal_A;
+
+    [SerializeField] GameObject Portal_B;
+
+    [SerializeField] Portal_Code_Triggers PCT_A;
+
+    [SerializeField] Portal_Code_Triggers PCT_B;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}

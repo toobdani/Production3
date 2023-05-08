183da971be728178c2e1a9179988d0bae1c3d6db
Daniel Tooby <danieltooby@yahoo.com>
2023-05-08 23:41:24 +0100

    Added First Room

---
+++ Assets/Scripts/Note_Code.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note_Code : MonoBehaviour
+{
+
+    [SerializeField] GameObject NoteUI;
+
+    [SerializeField] movementTest MT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NoteUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(NoteUI.activeSelf == true)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                NoteUI.SetActive(false);
+                MT.MoveAllow = 0;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NoteUI.SetActive(true);
+        MT.MoveAllow = 1;
+    }
+}
